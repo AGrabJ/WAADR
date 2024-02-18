d77aac8bcfb615993befb4d173f995c6ed39b9cf
gecheverria2020 <gecheverria2020@fau.edu>
2024-02-18 00:25:31 -0500

    button input testing file ver 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <stdio.h>
+#include <bcm2835>
+
+#define PIN RPI_GPIO_P1_16
 
 int main(){
-    std::cout<<"hello world"<<std::endl;
+    if(!bcm2835_init()){
+        return 1;
+    }
+
+    bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_set_pud(PIN, BCM2835_GPIO_PUD_UP);
+
+    while(1){
+        uint8_t value = bcm2835_gpio_lev(PIN);
+        printf("read from pin 15: %d\n", value);
+        // wait a bit
+        delay(500);
+    }
+
+    bcm2835_close();
     return 0;
 }
