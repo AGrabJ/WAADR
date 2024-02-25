4a23b9994dda2ab9891738bb9599b226b9f21040
gecheverria2020 <gecheverria2020@fau.edu>
2024-02-25 01:49:59 -0500

    including led-matrix.h test

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@
 #include <thread>
 #include <stdio.h>
 #include <wiringPi.h>
+#include "led-matrix.h"
 
 #define PIN_BUTTON 16
 
 int main(){
     wiringPiSetupGpio();
     pinMode(PIN_BUTTON, INPUT);
+
     std::cout<<"SET UP PIN 16"<<std::endl;
+
     while(1){
         if(digitalRead(PIN_BUTTON) == HIGH){
             std::cout<<"PRESSED!"<<std::endl;
